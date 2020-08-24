94633e2eb6bf2981bba77671ae6e5baf552346a7
bruno passarelli <52680954+brunopass@users.noreply.github.com>
2020-08-24 14:05:52 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -4,7 +4,9 @@ const config = {
     port: process.env.PORT,
     secret: process.env.SECRET,
     mongo_db: process.env.MONGO_DB_SECRET,
-    jwt: process.env.SECRET
+    jwt: process.env.SECRET,
+    mailjetKey: process.env.MAILJET_KEY,
+    mailjetSecret: process.env.MAILJET_SECRET
 }
 
 module.exports = {config}
